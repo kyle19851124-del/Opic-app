5500c73d68469a315e4d40e4097d8fd138e958c1
kyle19851124-del <kyle19851124@gmail.com>
2026-06-24 04:33:32 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "opic-pwa-v10-2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./data.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./baekho-smile.png",
+  "./baekho-rest.png",
+  "./baekho-focus.png",
+  "./baekho-mascot-set.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    })))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const clone = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone)).catch(() => null);
+      return response;
+    }).catch(() => cached))
+  );
+});
