a72169903dd3540c50425df3eda481bad639e668
kyle19851124-del <kyle19851124@gmail.com>
2026-06-24 04:30:27 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,38 @@
-const CACHE_NAME="opic-pwa-v10-0";
-const FILES_TO_CACHE=["./","./index.html","./data.js","./manifest.json","./icon-192.png","./icon-512.png","./baekho-smile.png","./baekho-rest.png","./baekho-focus.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE_NAME = "opic-pwa-v10-2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./data.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./baekho-smile.png",
+  "./baekho-rest.png",
+  "./baekho-focus.png",
+  "./baekho-mascot-set.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    })))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const clone = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone)).catch(() => null);
+      return response;
+    }).catch(() => cached))
+  );
+});
