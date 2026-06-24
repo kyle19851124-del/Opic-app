59f58ee03f7f3edb5abf935433384073085fe2d0
kyle19851124-del <kyle19851124@gmail.com>
2026-06-24 15:12:49 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
-const CACHE_NAME = "opic-pwa-v11-5";
-const ASSETS = ["./","./index.html","./data.js","./manifest.json","./icon-192.png","./icon-512.png","./baekho-smile.png","./baekho-rest.png","./baekho-focus.png","./baekho-listen.png","./baekho-mascot-set.png"];
+const CACHE_NAME = "opic-pwa-v11-6";
+const ASSETS = [
+"./","./index.html","./data.js","./manifest.json","./icon-192.png","./icon-512.png",
+"./baekho-smile.png","./baekho-rest.png","./baekho-focus.png","./baekho-listen.png","./baekho-mascot-set.png",
+"./baekho-hello.png","./baekho-walk.png","./baekho-curious.png","./baekho-sleepy.png","./baekho-excited.png","./baekho-exercise.png"
+];
 self.addEventListener("install", event => {event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null));self.skipWaiting();});
 self.addEventListener("activate", event => {event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => {if (key !== CACHE_NAME) return caches.delete(key);})))) ;self.clients.claim();});
 self.addEventListener("fetch", event => {if (event.request.method !== "GET") return;event.respondWith(fetch(event.request).then(response => {const clone=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,clone)).catch(()=>null);return response;}).catch(()=>caches.match(event.request)));});
